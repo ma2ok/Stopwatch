159e0e2341f78ed10b3b1322cd41c224770bc7a5
Tubasa Matsuoka <workwing0283@gamil.com>
2020-04-15 17:52:54 +0900

    細かい修正とStop Resetの追加

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 {
   const timer = document.getElementById('timer');
@@ -7,22 +7,31 @@
   const reset = document.getElementById('reset');
 
   let startTime;
+  let timeoutId;
 
   function countUp(){
-   const d = new Date(Date.now()- startTime);
-   const m = String(d.getMinutes()).padStart(2, '0');
-   const s = String(d.getSeconds()).padStart(2, '0');
-   const ms =String(d.getMilliseconds()).padStart(3,'0');
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3,'0');
 
-   timer.textContent = `${m}:${s}.${ms}`;
+    timer.textContent = `${m}:${s}.${ms}`;
 
-    setTimeout(() =>{
-      countUp()
+    timeoutId = setTimeout(() => {
+      countUp();
     }, 10);
   }
 
   start.addEventListener('click', () => {
     startTime = Date.now();
     countUp();
+  });
+
+  stop.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+  });
+
+  reset.addEventListener('click', () => {
+    timer.textContent = '00:00.000';
   });
 }
